14bb1b83eb508b84c9b2f29a39edb6795d3a6727
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-02-15 18:09:38 +0100

    polished theme behavior to change some button colors accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -6,41 +6,47 @@ import Articles from './components/Articles'
 import MyFooter from './components/MyFooter'
 import ArticleDetail from './components/ArticleDetail'
 import Header from './components/Header'
+import { useState } from 'react'
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-            <Header />
-            <Home />
-            <Articles />
-            <MyFooter />
-          </>
-        }
-      />
-      <Route
-        path="/article/:articleId"
-        element={
-          <>
-            <Header />
-            <ArticleDetail />
-            <MyFooter />
-          </>
-        }
-      />
-      <Route
-        path="*"
-        element={
-          <h1 className="text-center mt-5 text-danger fw-bold">
-            404 - Page not found.
-          </h1>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
-)
+const App = () => {
+  const html = document.getElementsByTagName('html')[0]
+  const [theme, setTheme] = useState(html.getAttribute('data-bs-theme')!)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header setTheme={setTheme} />
+              <Home />
+              <Articles theme={theme} />
+              <MyFooter />
+            </>
+          }
+        />
+        <Route
+          path="/article/:articleId"
+          element={
+            <>
+              <Header setTheme={setTheme} />
+              <ArticleDetail theme={theme} />
+              <MyFooter />
+            </>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <h1 className="text-center mt-5 text-danger fw-bold">
+              404 - Page not found.
+            </h1>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
